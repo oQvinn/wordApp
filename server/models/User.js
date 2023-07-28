6bce5d2b1102ec6c374e6cc3a47742a11117c639
oQvinn <wegnerq@gmail.com>
2023-07-28 14:31:50 -0600

    Starting application framework done

---
+++ server/models/User.js
@@ -0,0 +1,51 @@
+import mongoose from "mongoose";
+
+const UserSchema = new mongoose.Schema(
+  {
+    firstName: {
+      type: String,
+      required: true,
+      min: 2,
+      max: 50,
+    },
+    lastName: {
+      type: String,
+      required: true,
+      min: 2,
+      max: 50,
+    },
+    userName: {
+      type: String,
+      required: true,
+      unique: true,
+      max: 12,
+    },
+    email: {
+      type: String,
+      required: true,
+      max: 50,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      min: 5,
+    },
+    picturePath: {
+      type: String,
+      default: "",
+    },
+    friends: {
+      type: Array,
+      default: [],
+    },
+    location: String,
+    lifetimeScore: Number,
+    dailyScore: Number,
+    timesPlayed: Number,
+  },
+  { timestamps: true }
+);
+
+const User = mongoose.model("User", UserSchema);
+export default User;
